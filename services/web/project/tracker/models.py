4f2d5c592e7d9cd28d1a6596a0dd29a3f4d8f428
Peter Vuyk <peter.vuyk456@gmail.com>
2020-09-25 23:51:32 -0400

    Made changes to tracker page

--- services/web/project/tracker/models.py
+++ services/web/project/tracker/models.py
@@ -1,16 +1,36 @@
 import datetime
 from project import db
 
+accounts = db.Table('accounts',
+                    db.Column('user_id', db.Integer, db.ForeignKey(
+                        'user.id'), primary_key=True),
+                    db.Column('account_id', db.Integer, db.ForeignKey(
+                        'account.id'), primary_key=True)
+                    )
+
 
 class User(db.Model):
-    __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    role = db.Column(db.String(64), nullable=False, default="user")
     created_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    accounts = db.relationship('Account', secondary=accounts, lazy='subquery',
+                               backref=db.backref('users', lazy=True))
 
-    def __init__(self, username):
+    def __init__(self, username, role="user"):
         self.username = username
+        self.role = role
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    acct_type = db.Column(db.String(24), nullable=False)
+    history = db.Column(db.PickleType())
+
+    def __init__(self, name, acct_type):
+        self.name = name
+        self.acct_type = acct_type
